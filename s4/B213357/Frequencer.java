95846afe320c375e1f11e5813dfefe587cf2c7f8
tut213357 <higashimoto.kazuki.xg@tut.ac.jp>
2021-12-21 17:52:44 +0900

    12/21

--- s4/B213357/Frequencer.java
+++ s4/B213357/Frequencer.java
@@ -129,6 +129,63 @@ public class Frequencer implements FrequencerInterface{
         //   suffixArray[ 2]= 0:CBA
         // のようになるべきである。
 
+        // suffixCompareを用いて比較
+        // マージソート (参考: https://qiita.com/ta7uw/items/fbe35038436bbffea9d6)
+        // 平均計算量： O(nlog n)
+        // 整列されていないリストを2つのサブリストに分割する
+        // サブリストを整列する
+        // サブリストをマージしてひとつの整列済みリストにする
+
+        public class mergeSort {
+
+            public static void sort(int[] array, int low, int high){
+                if(low < high){
+                    int middle = (low + high) >>> 1;
+                    mergeSort.sort(array, low , middle);
+                    mergeSort.sort(array, middle+1, high);
+                    mergeSort.merge(array, low, middle, high);
+                }
+            }
+
+            public static void merge(int[] array, int low, int middle, int high){
+                int[] helper = new int[array.length];
+
+                for (int i = low; i <= high; i++){
+                    helper[i] = array[i];
+                }
+                int helperLeft = low;
+                int helperRight = middle + 1;
+                int current = low;
+
+                while (helperLeft <= middle && helperRight <= high){
+                    if (helper[helperLeft] <= helper[helperRight]){
+                        array[current] = helper[helperLeft];
+                        helperLeft ++;
+                    }
+                    else {
+                        array[current] = helper[helperRight];
+                        helperRight ++;
+                    }
+                    current ++;
+                }
+                int remaining = middle - helperLeft;
+                for (int i = 0; i <= remaining; i++){
+                    array[current + i] = helper[helperLeft + i];
+                }
+            }
+
+            public static void main(String[] args){
+                int[] test
+                        = { 10, 75, 24, 32, 98,
+                        72, 88, 43, 60, 35,
+                        54, 62,  2, 12, 82,
+                };
+                mergeSort.sort(test, 0, test.length-1);
+                for( int i=0; i<test.length; i++ ) {
+                    System.out.println( (i+1) + ":" + test[i] );
+                }
+            }
+        }
     }
 
     // ここから始まり、指定する範囲までは変更してはならないコードである。
